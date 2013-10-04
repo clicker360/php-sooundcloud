03de7a8d584248a882dd6184532802157ae980a1
Iram Gutiérrez <iram@clicker360.com>
2013-10-04 18:22:00 -0500

    index

--- index.php
+++ index.php
@@ -1,13 +1,13 @@
 <?php 
 
 require_once 'Services/Soundcloud.php';
-
-$soundcloud = new Services_Soundcloud(
-        '57af796c4b37011fd36335f74b90fc04', 
-        '22ae5d25caad69e9fd59d87756146f99', 
-        'http://dev.clicker.com/php-soundcloud'
-        );
-$token = $soundcloud->getAccessTokenUrl();
-echo $token;
+$sc_client_id = "57af796c4b37011fd36335f74b90fc04"; 
+$sc_secret = "22ae5d25caad69e9fd59d87756146f99";
+$sc_user = "iramgutzglez";
+$sc_pass = "sofiia";
+// create client object and set access token
+$client = new Services_Soundcloud($sc_client_id, $sc_secret);
+// login
+$client->credentialsFlow($sc_user, $sc_pass);
 ?>
 
