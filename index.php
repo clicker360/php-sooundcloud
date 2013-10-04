e8901d927598c08662f65ab568c0ff697674c1cf
Iram Gutiérrez <iram@clicker360.com>
2013-10-04 14:42:28 -0500

    index

--- index.php
+++ index.php
@@ -1,1 +1,13 @@
-<?php ?>
+<?php 
+
+require_once 'Services/Soundcloud.php';
+
+$soundcloud = new Services_Soundcloud(
+        '57af796c4b37011fd36335f74b90fc04', 
+        '22ae5d25caad69e9fd59d87756146f99', 
+        'http://dev.clicker.com/php-soundcloud'
+        );
+$authorizeUrl = $soundcloud->getAuthorizeUrl();
+
+?>
+<a href="<?php echo $authorizeUrl; ?>">Connect with SoundCloud</a>
