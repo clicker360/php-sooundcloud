c02ef1ecb52110dc6f4ae0b552841b7f58f595f0
Iram Gutiérrez <iram@clicker360.com>
2013-10-07 12:36:18 -0500

    sound

--- index.php
+++ index.php
@@ -29,7 +29,7 @@ $sc_pass = "sofiia";
 $client = new Services_Soundcloud($sc_client_id, $sc_secret);
 //$client->setAccessToken('YOUR_ACCESS_TOKEN');
 $user = $client->credentialsFlow($sc_user, $sc_pass);
-$track = json_decode($client->post('tracks', array(
+/*$track = json_decode($client->post('tracks', array(
     'track[title]' => 'test',
     //'track[asset_data]' => '/home/clickerdev/sites/dev.clicker360.com/public_html/php-soundcloud/test.mp3'
     'track[asset_data]' => '/var/www/html/php-soundcloud/test.mp3'
